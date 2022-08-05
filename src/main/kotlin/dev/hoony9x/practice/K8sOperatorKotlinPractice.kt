af13e8acc9576ee8b5850c2bfde6eeb166ff182a
Kihoon Han <me@hoony9x.dev>
2022-08-05 10:48:20 +0900

    Add basic codes

--- src/main/kotlin/dev/hoony9x/practice/K8sOperatorKotlinPractice.kt
+++ src/main/kotlin/dev/hoony9x/practice/K8sOperatorKotlinPractice.kt
@@ -1,10 +1,37 @@
 package dev.hoony9x.practice
 
+import io.fabric8.kubernetes.client.ConfigBuilder
+import io.fabric8.kubernetes.client.DefaultKubernetesClient
+import io.javaoperatorsdk.operator.Operator
+import io.javaoperatorsdk.operator.monitoring.micrometer.MicrometerMetrics
+import io.micrometer.core.instrument.logging.LoggingMeterRegistry
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.takes.facets.fork.FkRegex
+import org.takes.facets.fork.TkFork
+import org.takes.http.Exit
+import org.takes.http.FtBasic
+
 class K8sOperatorKotlinPractice {
     companion object {
+        private val log: Logger = LoggerFactory.getLogger(K8sOperatorKotlinPractice::class.java)
+
         @JvmStatic
         fun main(args: Array<String>) {
+            log.info("Practice Operator (nginx) starting...")
 
+            val config = ConfigBuilder().withNamespace("operator-practice").build()
+            val kubernetesClient = DefaultKubernetesClient(config)
+            val operator = Operator(kubernetesClient) { op ->
+                op.withMetrics(MicrometerMetrics(LoggingMeterRegistry()))
+            }
+
+            // TODO: Add Operator with Reconciler
+
+            operator.installShutdownHook()
+            operator.start()
+
+            FtBasic(TkFork(FkRegex("/health", "ALL GOOD!")), 8080).start(Exit.NEVER)
         }
     }
 }
