a03582eb292b027657a4519b2c044c89b21c4863
Kihoon Han <me@hoony9x.dev>
2022-08-05 10:43:53 +0900

    Add required libraries and log4j setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,19 @@ repositories {
 }
 
 dependencies {
+    implementation("io.javaoperatorsdk:operator-framework:3.1.0")
+    implementation("io.javaoperatorsdk:micrometer-support:3.1.0")
+    implementation("org.takes:takes:1.21.1")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.18.0")
+    implementation("org.slf4j:slf4j-api:1.7.36")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.3")
+
+    annotationProcessor("io.fabric8:crd-generator-apt:6.0.0")
+
     testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("io.javaoperatorsdk:operator-framework-junit-5:3.1.0")
 }
 
 tasks.test {
---
+++ src/main/kotlin/dev/hoony9x/practice/K8sOperatorKotlinPractice.kt
@@ -0,0 +1,10 @@
+package dev.hoony9x.practice
+
+class K8sOperatorKotlinPractice {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+        }
+    }
+}
